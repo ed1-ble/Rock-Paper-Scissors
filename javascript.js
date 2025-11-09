6aa89b29401099e1970382cdbb7b892c7d55b66a
ed1-ble <icrushturtslikemario@gmail.com>
2025-11-09 21:57:45 +0800

    RPS

--- javascript.js
+++ javascript.js
@@ -1,77 +1,55 @@
 const choices = ['Rock','Paper','Scissors']
 
-let getCompChoice = () => {
-    return choices[Math.floor(Math.random()*3)];
-}
-
-let getHumanChoice = () => {
-    let choice = prompt("What's your choice?");
-    return choice;
-}
-
-function playRound(compChoice, humChoice) {
-    switch(humChoice) {
-        case choices[0]:
-            if (compChoice === choices[1]) {
-                return 0;
-            } else if (compChoice === choices[2]) {
-                return 1;
-            } else {
-                return 2;
-            }
-            break;
-        case choices[1]:
-            if (compChoice === choices[0]) {
-                return 1;
-            } else if (compChoice === choices[2]) {
-                return 0;
-            } else {
-                return 2;
-            }
-            break;
-        case choices[2]:
-            if (compChoice === choices[0]) {
-                return 0;
-            } else if (compChoice === choices[1]) {
-                return 1;
-            } else {
-                return 2;
-            }
-            break;
-        
-    }
-}
-
-console.log('Hi im working')
 let score = 0;
 let compScore = 0;
+let rounds = 0;
 
-for (let i = 0; i <5; i++) {
-    let compC = getCompChoice();
-    let humC = getHumanChoice();
-    let status = playRound(compC,humC);
-    console.log(compC + ' VS ' + humC + ', ');
-    switch (status) {
-        case 1:
-            console.log('You win this round!');
-            score +=1;
-            break;
-        case 0:
-            compScore +=1;
-            console.log('You lose this round!');
-            break;
-        case 2:
-            console.log('This round is a draw!');
-            break;
-        default:
-            console.log('Invalid');
+let getCompChoice = () => {
+    return choices[Math.floor(Math.random()*choices.length)];
+};
+
+let addElement = (etype,parent,styles,content) => {
+    let e = document.createElement(etype);
+    e.textContent = content;
+    e.setAttribute('style',styles);
+    parent.appendChild(e);
+    return e;
+};
+
+const playRound = (human, comp) => {
+    if (human === comp) return "That's a draw!";
+    if (
+        (human === 'Rock' && comp === 'Scissors') ||
+        (human === 'Paper' && comp === 'Rock') ||
+        (human === 'Scissors' && comp === 'Paper')
+    ) return "You win!";
+    return "You lose!";
+};
+
+function onClick(humChoice) {
+
+    if (rounds > 5) {
+        return;
+    } else if (rounds === 5) {
+        rounds++;
+        addElement('h1',document.querySelector('div'),'color:blue',score > compScore ? 'VICTORY' : 'DEFEAT');
+        return;
     }
-} 
 
-if (score > compScore) {
-    console.log('YOU WIN!!')
-} else if (compScore >score) {
-    console.log('YOU LOSEEE!!!')
-} else {
-    console.log("IT'S A TIE!!")
+    let result = playRound(humChoice,getCompChoice());
+    if (result == 'You win!') {
+        score++;
+    } else if (result == 'You lose!') {
+        compScore++;
+    }
+    addElement('h2',document.querySelector('div'),'color:grey;',result);
+    rounds++;
+    return;
 }
+
+document.querySelectorAll('button').forEach((button)=>{
+    button.addEventListener('click',()=>{
+        onClick(button.getAttribute('id'));
+    });
+});
+
